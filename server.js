a2b7c2873a5c85ef8a9eb60d064b45af4295ea73
Andrew Zimmerman <andrew.zimmerman@reverb.com>
2018-03-28 11:30:52 -0500

    removes unused code

--- server.js
+++ server.js
@@ -10,13 +10,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
-// const passport = require('passport');
-// const session = require('express-session');
-
 
 var app = express();
 require('dotenv').load();
-// require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -27,26 +23,7 @@ app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-// app.use(session({
-// 	secret: 'secretClementine',
-// 	resave: false,
-// 	saveUninitialized: true
-// }));
-//
-// app.use(passport.initialize());
-// app.use(passport.session());
-
 routes(app);
-
-// var io = require('socket.io')(80);
-//
-// io.on('connection', function (socket) {
-//   console.log('connected');
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
 
 var port = process.env.PORT || 8080;
 
