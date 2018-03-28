a2370de6232dfcb6d32d7d8c0e154ed733b7157d
Andrew Zimmerman <andrew.zimmerman@reverb.com>
2018-03-28 11:50:40 -0500

    resets ws port on server

--- app/routes/index.js
+++ app/routes/index.js
@@ -20,7 +20,7 @@ module.exports = function (app) {
 		});
 
   var WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer();
+    wss = new WebSocketServer({port: 40510});
 
     wss.on('connection', function (ws) {
       console.log('client connected');
